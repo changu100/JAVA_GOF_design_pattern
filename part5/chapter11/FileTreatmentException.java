cad165adc71baf01324f11f00fbc1ae2eeaf2a70
changyu <changyu.park1@gmail.com>
2021-10-09 20:52:36 +0900

    composite 그릇과 내용물을 동일시하기

---
+++ part5/chapter11/FileTreatmentException.java
@@ -0,0 +1,9 @@
+package part5.chapter11;
+
+public class FileTreatmentException extends RuntimeException {      
+    public FileTreatmentException() {
+    }
+    public FileTreatmentException(String msg) {
+        super(msg);
+    }
+}

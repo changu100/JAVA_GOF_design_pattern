049da8ee4d1dcea9839a9f10394f74e3f713bd45
changyu <changyu.park1@gmail.com>
2021-10-10 20:08:40 +0900

    visitor pattern

---
+++ part6/chapter13/Element.java
@@ -0,0 +1,5 @@
+package part6.chapter13;
+
+public interface Element {
+    public abstract void accept(Visitor v);
+}
---
+++ part6/chapter13/FileTreatmentException.java
@@ -0,0 +1,9 @@
+package part6.chapter13;
+
+public class FileTreatmentException extends RuntimeException {      
+    public FileTreatmentException() {
+    }
+    public FileTreatmentException(String msg) {
+        super(msg);
+    }
+}
